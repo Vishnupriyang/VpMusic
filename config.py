e3810ca632706e2d7b1111512a06666a285baf75
Vishnu Priyang <freefireffofficial555@gmail.com>
2023-06-04 08:13:53 +0530

    Vp Music

---
+++ config.py
@@ -0,0 +1,27 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+
+OWNER_ID = int(getenv("OWNER_ID"))
+
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/75576017da241ed4e803c.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/75576017da241ed4e803c.jpg")
+
+SESSION = getenv("SESSION", None)
+
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/vpvpofficial")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/vpvpofficial")
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1001873246625").split()))
+
+
+FAILED = "https://telegra.ph/file/75576017da241ed4e803c.jpg"
